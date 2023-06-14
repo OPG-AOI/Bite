ee8822336944a60bcc9f635b1e50d3e7553470a2
Leather128 <81660872+Leather128@users.noreply.github.com>
2023-06-14 17:46:06 -0400

    added warn command and better ping command

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+# token
 import bot_token
 
+# randomness
+import random
+
+# time stuff
+from datetime import datetime
+from datetime import timedelta
+
+# discord stuff
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -10,8 +19,10 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="/", intents=intents)
 
+# initialization
 @bot.event
 async def on_ready():
+	random.seed()
 	print(f"Bot is ready. Logged in as {bot.user.name}")
 	
 	try:
@@ -20,13 +31,34 @@ async def on_ready():
 	except Exception as e:
 		print(e)
 
+# join message :3
 @bot.event
 async def on_member_join(member):
 	welcome_message = "Hello, Welcome to Bamboo's Fan Server! I hope you have a great time!"
 	await member.send(welcome_message)
 
+# ping command
 @bot.tree.command(name="ping", description="Pong!")
 async def ping(interaction: discord.Interaction):
+	before = datetime.now()
 	await interaction.response.send_message("Pong!", ephemeral=True)
 
+	now = datetime.now()
+	formatted_ping = "{:.2f}".format(((now.second * 1000.0 + now.microsecond) - (before.second * 1000.0 + before.microsecond)) / 1000.0)
+	await interaction.edit_original_response(content=f"Pong! ({formatted_ping} ms)")
+
+# warn command
+possible_warn_reasons = [
+	"They were acting like a monkey and got a timeout!",
+	"They were being too silly of a goose."
+]
+
+@bot.tree.command(name="warn", description="Warn a specific user with a funny message.")
+@app_commands.describe(user="User to warn.")
+@app_commands.default_permissions(administrator=True)
+async def warn(interaction: discord.Interaction, user: discord.Member):
+	await user.timeout(timedelta(minutes=5))
+	await interaction.response.send_message(f"{user.mention} has been warned! {possible_warn_reasons[random.randint(0, len(possible_warn_reasons) - 1)]}")
+
+# running the bot
 bot.run(bot_token.bot_token)
